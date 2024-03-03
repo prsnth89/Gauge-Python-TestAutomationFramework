00e6073af290ac1f0d22aeb483bb33e30d10d1cc
prsnth89 <prsnth.jobs@gmail.com>
2024-03-03 15:58:58 +0000

    updating logics

--- business_functions/step_impl/api_step_implementation.py
+++ business_functions/step_impl/api_step_implementation.py
@@ -9,13 +9,22 @@ def get_the_weather():
     rest_action=RestActions()
     rest_action.get_request(url="https://reqres.in/api/users?page=2")
     print(rest_action.status_code)
-   # print(rest_action.ret_json)
     all_data=rest_action.ret_json['data']
-    test_id={}
+    new_employee_list=[]
     for data in all_data:
         print(data)
-        test_id(data.get('id'),data.get('email'))
-    print(test_id)
+        emp_id=data.get('id')
+        email=data.get('email')
+        if emp_id is not None and email is not None:
+            employee_tuple=(emp_id,email)
+        else:
+            print("Missing id or email---",data)
+        
+        new_employee_list.append(employee_tuple)
+    print(new_employee_list)
+
+      
+
 
 @step("Validate response code <status_code>")
 def validate_response_code(status_code):
