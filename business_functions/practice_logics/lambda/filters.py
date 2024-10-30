902f22908ad366dc388a3243b5d9f6e31916c77a
prsnth89 <prsnth.jobs@gmail.com>
2024-10-30 10:49:14 +0000

    updating logics

--- business_functions/practice_logics/lambda/filters.py
+++ business_functions/practice_logics/lambda/filters.py
@@ -1,3 +1,7 @@
 numbers=[1,2,3,4,5,6,7,8,9]
 even_nos=filter(lambda x:x%2==0,numbers)
 print(list(even_nos))
+
+
+odd_nos=filter(lambda y:y%2!=0,numbers)
+print(list(odd_nos))
---
+++ business_functions/practice_logics/regular_expressions/reg.py
@@ -0,0 +1,31 @@
+import re
+name="prasanth@gopalakrishnan!"
+name1="Prasanth123Gopala887"
+name2="hello how are you Aswath!! well i am good"
+
+
+def find_spl_char(data):
+    reg_exp=re.findall(r'[^a-zA-Z0-9]',data)
+    new_char=''.join(reg_exp)
+    return new_char
+
+def find_no(data):
+    reg_exp=re.findall(r'[0-9]',data)
+    new_int=''.join(reg_exp)
+    return new_int
+
+def find_char(data):
+    reg_exp=re.findall(r'[a-zA-Z]',data)
+    new_str=''.join(reg_exp)
+    return new_str
+
+def find_4_digit_char(data):
+    reg_exp=re.findall(r'\b\w{4}\b',data)
+    new_str=' '.join(reg_exp)
+    return new_str
+
+
+print(find_spl_char(name))
+print(find_no(name1))
+print(find_char(name1))
+print(find_4_digit_char(name2))
