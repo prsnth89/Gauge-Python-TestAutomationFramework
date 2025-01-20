3427e9f2078c91e8910a18f5d4354329af6dfd52
prsnth89 <prsnth.jobs@gmail.com>
2025-01-20 20:35:42 +0000

    updating regex

--- business_functions/practice_logics/regular_expressions/reg.py
+++ business_functions/practice_logics/regular_expressions/reg.py
@@ -24,8 +24,14 @@ def find_4_digit_char(data):
     new_str=' '.join(reg_exp)
     return new_str
 
+def find_vowels(data):
+    reg_ex=re.findall(r'[aeiouAEIOU]',data)
+    new_str=''.join(reg_ex)
+    return new_str
+
 
 print(find_spl_char(name))
 print(find_no(name1))
 print(find_char(name1))
 print(find_4_digit_char(name2))
+print("vowels----",find_vowels(name1))
